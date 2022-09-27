7d7d166294bf53d6e7d16d831c93f54e15203e5f
Rita Umukoro <liliphyzie002@gmail.com>
2022-09-27 12:18:32 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,11 +1,5 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
-
-/**
- * void prototypes()
- * int prototypes()
- */
-
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
@@ -16,5 +10,4 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
-
 #endif
